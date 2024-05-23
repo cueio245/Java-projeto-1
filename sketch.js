7836d960161106451303c3c3c496038fdcaecd39
cueio245 <00001105714159sp@al.educacao.sp.gov.br>
2024-05-23 16:22:38 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(600, 500);
+  background("blue");
+}
+
+function draw() {
+  fill("red");
+  stroke("lightblue");
+  if (mouseIsPressed);
+  circle(mouseX, mouseY, 30, 50);
+}
